64aa1a01ed35510793192071397e4556d51cd0fd
fanhucci <felipeanhucci38@gmail.com>
2024-05-15 23:27:17 -0300

    adição do path

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const porta = 4000;
 const host = '0.0.0.0';
 
 const app = express();
 
-app.use(express.static('./publico'));
+app.use(express.static(path.join(process.cwd(),'./publico')));
 
 app.listen(porta, host,() =>{
     console.log(`Servidor executando na porta http://${host}:${porta}`);
